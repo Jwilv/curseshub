c7c2e651425c78728d7f8f0f46ca1232ec973dd4
Jwilv <juan.ignacio.wilvers@gmail.com>
2024-02-11 21:03:33 -0300

    SidebarRoutes

--- components/navegation/components/SidebarRoutes.tsx
+++ components/navegation/components/SidebarRoutes.tsx
@@ -1,6 +1,31 @@
+"use client";
+
+import { Compass, Layout } from "lucide-react";
+
+const guestRoutes = [
+    {
+        icon: Layout,
+        name: "Dashboard",
+        href: '/',
+    },
+    {
+        icon: Compass,
+        name: "Browse",
+        href: '/search',
+    }
+]
 
 export const SidebarRoutes = () => {
+
+    const routes = guestRoutes
+
     return (
-        <div>SidebarRoutes</div>
+        <div className="flex flex-col w-full">
+            {
+                routes.map((route) => (
+                    <SidebarItem {...route} />
+                ))
+            }
+        </div>
     )
 }
