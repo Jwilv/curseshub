448554db489f8ba7ad769244b51897582ee7094f
Jwilv <juan.ignacio.wilvers@gmail.com>
2024-02-13 23:09:33 -0300

    update DashboardLayout

--- components/navegation/index.ts
+++ components/navegation/index.ts
@@ -1,1 +1,5 @@
+export * from './Navbar';
 export { default as Sidebar } from './Sidebar';
+export { default as Brand } from './components/Brand';
+export * from './components/SidebarItem';
+export * from './components/SidebarRoutes';
